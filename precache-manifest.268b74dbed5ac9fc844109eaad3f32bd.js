c3f11d1e08ce9daaa110b60e17d72718cb536d64
Monde <monde.ngalonkulu@nimbletech.co.za>
2020-08-13 21:05:49 +0200

    Updates

---
+++ precache-manifest.268b74dbed5ac9fc844109eaad3f32bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0deba3fbea772fce204e747c6bb9ea",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "9d4c11924d69557b8cd1",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fd339e3d7876b514767a",
+    "url": "/face-recognition-brain/static/css/main.fb605388.chunk.css"
+  },
+  {
+    "revision": "9d4c11924d69557b8cd1",
+    "url": "/face-recognition-brain/static/js/2.3fa2e4ab.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.3fa2e4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd339e3d7876b514767a",
+    "url": "/face-recognition-brain/static/js/main.00ca8239.chunk.js"
+  },
+  {
+    "revision": "29e5821f791c973dcb04",
+    "url": "/face-recognition-brain/static/js/runtime-main.e3020e8e.js"
+  },
+  {
+    "revision": "b93de15451563620bb247c9e5a6a06a4",
+    "url": "/face-recognition-brain/static/media/brain-bw.b93de154.png"
+  }
+]);
